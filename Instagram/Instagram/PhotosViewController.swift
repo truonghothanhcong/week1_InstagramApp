c2d204be418ecf70860936199c5210409305f6ad
Un <ping.to.8888@gmail.com>
2016-10-12 19:48:52 +0700

    Phan 3

--- Instagram/Instagram/PhotosViewController.swift
+++ Instagram/Instagram/PhotosViewController.swift
@@ -8,12 +8,60 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    let refreshControl = UIRefreshControl()
+    
+    
+    var photos: [NSDictionary]?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        
+        
+        
+        
+        
+        let userId = "435569061"
+        let accessToken = "435569061.c66ada7.d12d19c8687e427591f254586e4f3e47"
+        let url = URL(string: "https://api.instagram.com/v1/users/\(userId)/media/recent/?access_token=\(accessToken)")
+        
+        if let url = url {
+            let request = URLRequest(
+                url: url,
+                cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData,
+                timeoutInterval: 10)
+            let session = URLSession(
+                configuration: URLSessionConfiguration.default,
+                delegate: nil,
+                delegateQueue: OperationQueue.main)
+            let task = session.dataTask(
+                with: request,
+                completionHandler: { (dataOrNil, response, error) in
+                    if let data = dataOrNil {
+                        if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            if let photoData = responseDictionary["data"] as? [NSDictionary] {
+                                self.photos = photoData
+                                self.tableView.reloadData()
+                                self.refreshControl.endRefreshing()
+                            }
+                        }
+                    }
+            })
+            task.resume()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +69,15 @@ class PhotosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
     /*
     // MARK: - Navigation
 
